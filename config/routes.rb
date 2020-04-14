fd9d9fe10535bced4dd116a1628b0d6ccae28bf4
jokale <kalemajoanna@gmail.com>
2020-04-14 11:27:30 +0100

    fix syntax error in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :body_parts, only: [:index, :show]
   resources :athletes do 
-    resources :workouts  only: [:index, :show]
+    resources :workouts,  only: [:index, :show]
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :workouts
